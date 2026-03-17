170ae6e83581c2252abe202c4d84ea63bedc0ad5
A4DREK <yael.adrian640305@gmail.com>
2026-03-17 13:52:44 -0600

    Batman Begins

---
+++ Sudoku.java
@@ -0,0 +1,20 @@
+public class Sudoku{
+
+  private int [][] tablero;
+
+  public Sudoku(int [][] tablero){
+    this.tablero = tablero;
+  }
+
+  public void resolverSudoku(){
+
+  }
+
+  private boolean posicionValida(int fila, int col, int num){
+    return true;
+  }
+
+  public void imprimeSolucion(){
+
+  }
+}
